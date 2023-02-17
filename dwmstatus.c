6254955ed59c5de962e805797f67fce8f89dc5e3
Matt Nakama <mattnakama@skytap.com>
2023-02-17 16:41:59 -0500

    add battery level, when there's a battery

--- dwmstatus.c
+++ dwmstatus.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
 //#include <stdarg.h>
 //#include <sys/time.h>
 #include <time.h>
@@ -11,11 +13,10 @@
 #include <X11/Xlib.h>
 
 static Display *dpy;
-static char timestr[255] = {0};
-static time_t secs = 0;
+static int batteryFile = 0;
 
 time_t
-mktimes(char *fmt)
+mktimes(char* timestr, int timestr_len, char *fmt)
 {
 	time_t tim;
 	struct tm *timtm;
@@ -27,7 +28,7 @@ mktimes(char *fmt)
 		exit(1);
 	}
 
-	if (!strftime(timestr, sizeof(timestr)-1, fmt, timtm)) {
+	if (!strftime(timestr, timestr_len, fmt, timtm)) {
 		fprintf(stderr, "strftime == 0\n");
 		exit(1);
 	}
@@ -43,25 +44,50 @@ setstatus(char *str)
 }
 
 int
+battery(char **statusstr_p, int statusstr_len)
+{
+	char *statusstr = *statusstr_p;
+	int digits = read(batteryFile, statusstr, statusstr_len);
+	lseek(batteryFile, 0, SEEK_SET);
+
+	statusstr[digits-1] = '%';
+	statusstr[digits] = ' ';
+	*statusstr_p += digits+1;
+
+	return digits;
+}
+
+int
 main(void)
 {
+	static time_t secs = 0;
+	static char timestr[255] = {0};
+
+	fclose(stdin);
+	batteryFile = open("/sys/class/power_supply/BAT0/capacity", O_RDONLY | O_CLOEXEC);
+
 	if (!(dpy = XOpenDisplay(NULL))) {
 		fprintf(stderr, "dwmstatus: cannot open display.\n");
 		return 1;
 	}
 
 	for (;;sleep(secs)) {
+		char* str = timestr;
+		if (batteryFile >= 0) {
+			battery(&str, sizeof(timestr));
+		}
+
 		//mktimes("%a %d %b %H:%M:%S %Z %Y");
 		//secs = 60 - (mktimes("%d  %H:%M:%S") % 60);
-		secs = 60 - (mktimes("%d  %H:%M") % 60);
+		secs = 60 - (mktimes(str, sizeof(timestr)-(str-timestr), "%d  %H:%M") % 60);
 
 		setstatus(timestr);
 	}
 
-	setstatus("dwm-6.1");
+	setstatus("dwm");
 
 	XCloseDisplay(dpy);
+	close(batteryFile);
 
 	return 0;
 }
-
